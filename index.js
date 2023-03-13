3718203a2612b42104f15d684c9b9fc986891035
Webs95 <93885891+Webs95@users.noreply.github.com>
2023-03-13 22:16:18 +0300

    feat: add console log

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const platform = window.navigator.platform;
 const userAgent = window.navigator.userAgent;
 
 const osList = [
-  {name: 'Windows', match: 'Win'},
-  {name: 'Android', match: 'Android'},
-  {name: 'iOS', match: 'iPhone|iPad|iPod'},
-  {name: 'Linux', match: 'Linux'},
-  {name: 'Mac OS', match: 'Mac'}
+  { name: 'Windows', match: 'Win' },
+  { name: 'Android', match: 'Android' },
+  { name: 'iOS', match: 'iPhone|iPad|iPod' },
+  { name: 'Linux', match: 'Linux' },
+  { name: 'Mac OS', match: 'Mac' },
 ];
 
-const detectedOs = osList.find(os => userAgent.match(new RegExp(os.match, 'i'))) || {name: 'Unknown'};
+const detectedOs = osList.find((os) =>
+  userAgent.match(new RegExp(os.match, 'i'))
+) || { name: 'Unknown' };
 const osName = detectedOs.name;
 
 userOs.innerText = osName;
-
+console.log('Your OS is:', osName);
