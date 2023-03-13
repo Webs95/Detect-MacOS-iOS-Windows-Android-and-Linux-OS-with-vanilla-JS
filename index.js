df2949ed56218605b1c58801d17ccdb3f20da9ce
Webs95 <93885891+Webs95@users.noreply.github.com>
2023-03-13 22:09:56 +0300

    refactor: code refactoring

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
 import './style.scss';
 
 const userOs = document.querySelector('.os');
+const platform = window.navigator.platform;
+const userAgent = window.navigator.userAgent;
 
-let os = 'unknow';
+const osList = [
+  {name: 'Windows', match: 'Win'},
+  {name: 'Android', match: 'Android'},
+  {name: 'iOS', match: 'iPhone|iPad|iPod'},
+  {name: 'Linux', match: 'Linux'},
+  {name: 'Mac OS', match: 'Mac'}
+];
 
-function getOS() {
-  const userAgent = window.navigator.userAgent;
-  const platform =
-    window.navigator?.userAgentData?.platform || window.navigator.platform;
-  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-  const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-  const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+const detectedOs = osList.find(os => userAgent.match(new RegExp(os.match, 'i'))) || {name: 'Unknown'};
+const osName = detectedOs.name;
 
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'Mac OS';
-  } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = 'iOS';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'Windows';
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android';
-  } else if (/Linux/.test(platform)) {
-    os = 'Linux';
-  }
+userOs.innerText = osName;
 
-  return os;
-}
-
-getOS();
-
-userOs.innerText = `${os}`;
-
-// const androidUrl =
-//   'intent://app.gigzy.net/#Intent;scheme=gigzy;package=com.gigzy.gigzyApp;end';
-// const iOsUrl =
-//   'https://apps.apple.com/ru/app/gigzy/id6443645741?platform=iphone';
-
-// if (os === 'Android') window.location.replace(androidUrl);
-// if (os === 'iOS') {
-//   window.location.replace("gigzy://"); 
-//   setTimeout(() => {
-//     window.location.replace(iOsUrl); 
-//   }, 10000);
-// }
